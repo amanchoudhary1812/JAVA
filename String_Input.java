498413d0e58c31d06c343ed19fe1fa1635a22069
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-01-16 16:24:16 +0530

    Commit

---
+++ String_Input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class String_Input {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        System.out.println(name);
+        String myname = input.next();
+        System.out.println(name);
+    }
+}
