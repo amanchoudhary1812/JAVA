f8f571e2089f844728295c2031ce28cb45413b8a
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-01-16 17:00:42 +0530

    Commit

---
+++ While_Loop.java
@@ -0,0 +1,14 @@
+public class While_Loop {
+    public static void main(String[] args) {
+        
+        int num = 5;
+        
+        while(num <= 10){
+
+            System.out.println(num);
+
+            num += 1;
+        }
+    }
+    
+}
