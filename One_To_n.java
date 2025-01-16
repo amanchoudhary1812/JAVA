094ed06e33fd67e170f165131914001221ab2d42
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-01-16 16:57:01 +0530

    Commit

---
+++ One_To_n.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class One_To_n {
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        for(int num = 1; num <= n; num += 1){
+            
+            System.out.print(num + " ");
+
+            System.out.println("Hello World");
+        }
+    }
+}
