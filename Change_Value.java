6dc4f5048a00955955d54ae4b08c1d936de6a87b
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-01-18 15:15:17 +0530

    Commit

---
+++ Change_Value.java
@@ -0,0 +1,15 @@
+
+import java.util.Arrays;
+
+
+public class Change_Value {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        change(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+        
+    static void change(int[]nums) {
+        nums[0] = 99;
+    }
+}
