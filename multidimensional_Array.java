d1fb0670ee0c01f6e1c808ffc46317359a4621c0
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-01-21 18:14:13 +0530

    Commit

---
+++ multidimensional_Array.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class multidimensional_Array {
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+        int[][] arr = new int [3][3];
+        System.out.println(arr.length); //No. of rows.
+
+        // Input
+        for (int row = 0; row < arr.length; row++) {
+            
+            // For each col in every row
+            for(int col = 0; col < arr[row].length; col++){
+                arr [row][col] = in.nextInt();
+            }
+        }
+
+        // Output
+        // for(int row = 0; row < arr.length; row++){
+
+        //     for(int col = 0; col < arr[row].length; col++){
+        //         System.out.print(arr [row][col] + " ");
+        //     }
+        //     System.out.println();
+        // }
+        
+        // // Output (Enhanced for loop)
+        // for (int row = 0; row < arr.length; row++){
+        //     System.out.println(Arrays.toString(arr[row]));
+        // }
+
+        // Output (Enhanced for loop)
+        for(int[] a : arr){    // For every element in array, print the element.
+            System.out.println(Arrays.toString(a));
+        }
+    }
+}
