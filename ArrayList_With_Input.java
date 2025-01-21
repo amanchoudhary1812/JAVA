a4d1d362fa7f5e147de3f4504875abb5543d6afe
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-01-21 19:07:01 +0530

    Commit

---
+++ ArrayList_With_Input.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class ArrayList_With_Input {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        ArrayList<Integer> list = new ArrayList<>(5);
+        
+        // Input
+        for(int i = 0; i<5; i++){
+            list.add(in.nextInt());
+        }
+
+        // Get item into index
+        for(int i = 0; i < 5; i++){
+            System.out.println(list.get(i));
+        }
+    }
+}
