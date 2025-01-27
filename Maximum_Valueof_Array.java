834dfb6b1536f7f4456c5ac94782f60428e572ca
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-01-27 22:37:28 +0530

    Commit

---
+++ Maximum_Valueof_Array.java
@@ -0,0 +1,36 @@
+public class Maximum_Valueof_Array{
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 2, 9, 18};
+        System.out.println(max(arr));
+        System.out.println(maxRange(arr, 3, 2));
+    }
+
+    static int max(int[] arr){
+        if(arr.length == 0){
+            return -1;
+        }
+        int maxVal = arr[0];
+        for(int i = 1; i < arr.length; i++){
+            if(arr[i] > maxVal){
+                maxVal = arr[i];
+            }
+        }
+        return maxVal;
+    }
+
+    static int maxRange (int[] arr, int start, int end){
+        if (end > start){
+            return -1;
+        }
+        if (arr == null){
+            return -1;
+        }
+        int maxVal = arr [start];
+        for(int i = start; i <= end; i++){
+            if(arr[i] > maxVal){
+                maxVal = arr[i];
+            }
+        }
+        return maxVal;
+    }
+}
