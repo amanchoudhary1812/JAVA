4696dff014186d5001c63a270f101e474568be84
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-02-21 01:03:38 +0530

    Commit

--- One_To_Five.java
+++ One_To_Five.java
@@ -3,7 +3,7 @@
 public class One_To_Five {
     public static void main(String[] args) {
         
-        for(int num = 10; num <= 15; num += 1){
+        for(int num = 1; num <= 5; num += 1){
 
             System.out.println(num);
 
