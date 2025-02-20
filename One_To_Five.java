0df82a0a7d74dd03997b98203b37a9e5abb1ac2f
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-02-21 01:03:17 +0530

    Commit

--- One_To_Five.java
+++ One_To_Five.java
@@ -1,11 +1,9 @@
 // Ques: Print numbers from 1 to 5
 
-import java.util.concurrent.ForkJoinPool;
-
 public class One_To_Five {
     public static void main(String[] args) {
         
-        for(int num = 1; num <= 5; num += 1){
+        for(int num = 10; num <= 15; num += 1){
 
             System.out.println(num);
 
