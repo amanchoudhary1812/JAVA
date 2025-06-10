f9eaa995e45e5c4883382fbb6745f57cefdea6ba
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-06-10 21:39:43 +0530

    Commit

---
+++ StringExample.java
@@ -0,0 +1,15 @@
+// Java program to demonstrate String creation
+
+public class StringExample {
+
+    public static void main(String[] args) {
+
+        // Creating string using string literal
+        String literalString = "GeeksforGeeks";
+        System.out.println("Using string literal: " + literalString);
+
+        // Creating string using new keyword
+        String objectString = new String("GeeksforGeeks");
+        System.out.println("Using new keyword: " + objectString);
+    }
+}
