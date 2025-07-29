42d026cc305827cbf7013fb2ada0baa6fc6249c2
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-07-29 16:29:39 +0530

    Commit

---
+++ RotationCount.java
@@ -0,0 +1,35 @@
+public class RotationCount {
+    public static void main(String[] args) {
+        int[] arr = {4,5,6,7,0,1,2};
+        System.out.println(countRotations(arr));
+    }
+
+    public static int countRotations(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            // If the array is already sorted
+            if (nums[start] <= nums[end]) {
+                return start;
+            }
+
+            int mid = start + (end - start) / 2;
+            int next = (mid + 1) % nums.length;
+            int prev = (mid - 1 + nums.length) % nums.length;
+
+            // Check if mid is the smallest
+            if (nums[mid] <= nums[next] && nums[mid] <= nums[prev]) {
+                return mid;
+            }
+
+            if (nums[mid] >= nums[start]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return 0; // No rotation
+    }
+}
