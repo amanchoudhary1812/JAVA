f55d46f671640422ae8f4f88915ccc8a5cd85776
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-08-01 22:50:14 +0530

    Commit

--- RowColMatrix.java
+++ RowColMatrix.java
@@ -7,7 +7,7 @@ public class RowColMatrix {
             {10, 20, 30, 40},
             {15, 25, 35, 45},
             {28, 29, 37, 49},
-            {33, 34, 38, 50}
+            {33, 34, 38, 60}
         };
 
         System.out.println(Arrays.toString(search(arr, 49)));
