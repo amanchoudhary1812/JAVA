265b058752db2e6f7a7c26e3ab34d151e571370e
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-08-02 21:14:33 +0530

    Commit

--- SplitArrayLargestSum.java
+++ SplitArrayLargestSum.java
@@ -1,7 +1,7 @@
 public class SplitArrayLargestSum {
     public static void main(String[] args) {
         int[] nums = {1,2,3,4,5};
-        int k = 5;
+        int k = 2;
 
         int ans = splitArray (nums, k);
         System.out.println(ans);
