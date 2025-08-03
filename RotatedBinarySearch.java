93473d99d7b7fbf8bca92b7c28f33b9e8c3baf6f
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-08-03 22:38:09 +0530

    Commit

--- RotatedBinarySearch.java
+++ RotatedBinarySearch.java
@@ -1,6 +1,6 @@
 public class RotatedBinarySearch {
     public static void main(String[] args) {
-        int[] nums = {4,5,6,7,0,1,2};
+        int[] nums = {4,5,6,7,0,1,2,3};
         int target = 0;
         int ans = search(nums, target);
         System.out.println(ans);
