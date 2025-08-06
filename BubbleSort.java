55fcc78df80605ab1094f92ed25c2719279c3d89
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-08-06 17:53:25 +0530

    Commit

--- BubbleSort.java
+++ BubbleSort.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] arr = {5, 4, 4, 2, 1};
+        int[] arr = {7, 21 , -23, 0, 77, -31, -5};
         bubble(arr);
         System.out.println(Arrays.toString(arr));
     }
