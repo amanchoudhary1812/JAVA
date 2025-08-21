688cb223215aab02e1e2c72e9ca1a7a2cedc3584
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2025-08-21 21:19:23 +0530

    Commit

--- Change_Value.java
+++ Change_Value.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class Change_Value {
     public static void main(String[] args) {
+        
         int[] arr = {1, 2, 3, 4, 5};
         change(arr);
         System.out.println(Arrays.toString(arr));
