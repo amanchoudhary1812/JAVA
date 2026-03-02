fedec51a20934122a1f2b5fdbf9c137fc96fcc02
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2026-03-02 22:04:23 +0530

    Commit

---
+++ LC2619_ArrayLast.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class LC2619_ArrayLast {
+
+    public static int last(int[] nums) {
+        if (nums.length == 0) {
+            return -1;
+        }
+        return nums[nums.length - 1];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // size of array
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        // array input
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int result = last(nums);
+        System.out.println("Last element: " + result);
+
+        sc.close();
+    }
+}
