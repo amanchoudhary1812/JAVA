59be93d502380ca29edf532e2fdd76554570dd14
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2026-03-24 10:46:50 +0530

    Commit

---
+++ VolumeOfSphere.java
@@ -0,0 +1,9 @@
+public class VolumeOfSphere {
+    public static void main(String[] args) {
+        double radius = 5;
+        double volume = 4.0/3.0 * 3.14 * radius * radius * radius;
+        
+        System.out.println(volume);
+
+    }
+}
