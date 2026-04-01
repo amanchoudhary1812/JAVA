4b52e1ed979e566e58038cc0bbc69e3418df1745
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2026-04-01 23:27:52 +0530

    Commit

--- DigitsOfNumber.java
+++ DigitsOfNumber.java
@@ -7,6 +7,7 @@ public class DigitsOfNumber {
         System.out.print("Enter your number : ");
 
         int n = sc.nextInt();
+        if (n == 0) n = 1;
         int count = 0;
 
         while (n != 0) {
---
+++ SumOfDigitsOfANumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SumOfDigitsOfANumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+        System.out.print("Enter number : ");
+        int n = sc.nextInt();
+        int sum = 0;
+
+        while (n != 0) {
+            int lastDigit = n%10;
+            sum += lastDigit;
+            n /= 10;
+        }
+        System.out.println(sum);
+    }
+}
