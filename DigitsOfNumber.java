eb3326a1dc38d64ab144cac01c2ce14d8f279552
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2026-04-01 20:42:30 +0530

    Commit

---
+++ DigitsOfNumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class DigitsOfNumber {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+        System.out.print("Enter your number : ");
+
+        int n = sc.nextInt();
+        int count = 0;
+
+        while (n != 0) {
+            n = n / 10;
+            count++;
+        }
+        System.out.println(count);
+
+    }
+}
