a779ab1a86d0dfe3f9416c74ed4936a162cd3fa7
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2026-04-02 10:43:56 +0530

    Commit

--- StarSquare.java
+++ StarSquare.java
@@ -5,13 +5,22 @@ public class StarSquare {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
 
-        System.out.print("Enter the number : ");
-        int row = sc.nextInt();
-        int col = row;
+        System.out.print("Enter the number : "); //  kisi bhi no. ka square (rows and columns are equal)
+        int n = sc.nextInt();
 
-        for (int i = 1; i <= row; i++) {
-            for (int j = 1; j <= col; j++) {
-                System.out.print("* ");
+        // Star printing
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = 1; j <= n; j++) {
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+
+        // Number Print
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print(j + " ");
             }
             System.out.println();
         }
