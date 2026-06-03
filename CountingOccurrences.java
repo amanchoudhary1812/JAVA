cd708ce5ba0a98718870f18120e3ed87e6fc590c
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2026-06-03 16:15:51 +0530

    Commit

---
+++ CountingOccurrences.java
@@ -0,0 +1,18 @@
+
+public class CountingOccurrences {
+    public static void main (String[] args) {
+        // Scanner sc = new Scanner (System.in);
+
+        int n = 1553557555;
+        int count = 0;
+
+        while (n > 0) {
+            int rem = n % 10;
+            if (rem == 5) {
+                count++;
+            }
+            n = n/10;
+        }
+        System.out.println(count);
+    }
+}
