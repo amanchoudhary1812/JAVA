c118fd0bd8cb4d21ddebbe59fdc0a54161e92700
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2026-06-03 16:09:56 +0530

    Commit

---
+++ LargestOfThreeNumbers.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class LargestOfThreeNumbers {
+    public static void main (String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+
+        int largest = a;
+
+        if (b > largest) {
+            largest = b;
+        }
+
+        if (c > largest) {
+            largest = c;
+        }
+
+        System.out.println(largest);
+
+    }
+}
