04b2e2478decb82b38844d05110a7ca7613fed44
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2026-06-05 22:06:37 +0530

    Commit

---
+++ MinimumNumberInArray.java
@@ -0,0 +1,18 @@
+public class MinimumNumberInArray {
+    public static void main(String[] args) {
+        int[] arr = {12, 18, -7, 3, 22, 56};
+
+        System.out.println(min(arr));
+    }
+
+    static int min (int[] arr) {
+        int min = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+}
