21b0959727eb91a9bfdcb5bc21116749850af8b4
amanchoudhary1812 <amanchoudhary1812@gmail.com>
2026-06-17 16:48:19 +0530

    Commit

---
+++ GCD_LCM.java
@@ -0,0 +1,16 @@
+public class GCD_LCM {
+    public static void main(String[] args) {
+        System.out.println(GCD(4,9));
+    }
+
+    static int GCD (int a, int b) {
+        if (a == 0) {
+            return b;
+        }
+        return GCD(b%a, a);
+    }
+
+    // static int LCM (int a, int b) {
+    //     return a*b / GCD (b%a, a);
+    // }
+}
